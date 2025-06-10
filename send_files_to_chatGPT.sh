3a5d6ab6b8a5c60673a2a9dbe0477b1e3b20e24b
Rob Nugen <rob@robnugen.com>
2025-06-10 17:36:37 +0900

    Easily ask Ai-chan about code

---
+++ send_files_to_chatGPT.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ "$#" -lt 1 ]; then
+    echo "Usage: $0 file1.php [file2.php ...]"
+    exit 1
+fi
+
+for file in "$@"; do
+    echo "Here is the source for $file:\n"
+    echo '```php'
+    cat "$file"
+    echo '```'
+    echo -e "\n------------------------\n"
+done
