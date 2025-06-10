bda2c31363d9a1f984ebcc9d3b921115c7a73dea
Rob Nugen <rob@robnugen.com>
2025-06-10 17:35:06 +0900

    Create random cookies (soon)

---
+++ classes/Utilities.php
@@ -0,0 +1,23 @@
+<?php
+
+class Utilities {
+
+    public static function randomString(
+        int $length,
+        string $possible = ""
+    ): string
+    {
+        $randString = "";
+        // define possible characters
+        if (empty($possible)) {
+            $possible = "0123456789abcdfghjkmnopqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ";
+        }
+        // add random characters
+        for ($i = 0; $i < $length; $i++) {
+            // pick a random character from the possible ones
+            $char = substr($possible, random_int(0, strlen($possible) - 1), 1);
+            $randString .= $char;
+        }
+        return $randString;
+    }
+}
