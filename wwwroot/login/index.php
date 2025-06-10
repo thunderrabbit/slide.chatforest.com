b2113424c305924c775ae439d2e0f1d2486ed635
Rob Nugen <rob@robnugen.com>
2025-06-10 17:41:14 +0900

    Simple login checker

---
+++ wwwroot/login/index.php
@@ -0,0 +1,14 @@
+<?php
+
+# Must include here because DH runs FastCGI https://www.phind.com/search?cache=zfj8o8igbqvaj8cm91wp1b7k
+include_once "/home/dh_fbrdk3/db.marbletrack3.com/prepend.php";
+
+if ($is_logged_in->isLoggedIn()) {
+    // We logged in.. yay!
+    header(header: "Location: /admin/");
+    exit;
+} else {
+    // We must log in.. yay!
+    header(header: "Location: /login/");
+    exit;
+}
