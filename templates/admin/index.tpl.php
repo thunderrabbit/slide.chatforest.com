8163a15d959793285e34987b0669fb89a8df87e3
Rob Nugen <rob@robnugen.com>
2025-06-12 00:21:19 +0900

    Can now log in wooooo

---
+++ templates/admin/index.tpl.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Welcome Boss!</title>
+        <meta name="title" content="Welcome Boss!"/>
+        <meta name="description" content=""/>
+        <link rel="stylesheet" href="/css/styles.css">
+    </head>
+    <body><!-- Welcome Boss! form area -->
+        <div class="PageWrapper">
+            <div class="PagePanel">
+                What's up Boss? <br />
+            </div>
+        </div>
+        <div class="fix"></div>
+    </body>
+</html>
---
+++ wwwroot/admin/index.php
@@ -0,0 +1,12 @@
+<?php
+
+# Must include here because DH runs FastCGI https://www.phind.com/search?cache=zfj8o8igbqvaj8cm91wp1b7k
+include_once "/home/dh_fbrdk3/db.marbletrack3.com/prepend.php";
+
+if ($is_logged_in->isLoggedIn()) {
+    // We logged in.. yay!
+    $page = new \Template(config: $config);
+    $page->setTemplate("admin/index.tpl.php");
+    $page->echoToScreen();
+    exit;
+}
