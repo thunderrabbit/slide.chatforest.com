0f114101886df8c37c7ff604bf399a5fe393afa5
Rob Nugen <rob@robnugen.com>
2025-06-12 20:18:11 +0900

    Embed content into base admin frame

--- wwwroot/admin/index.php
+++ wwwroot/admin/index.php
@@ -4,12 +4,17 @@
 include_once "/home/dh_fbrdk3/db.marbletrack3.com/prepend.php";
 
 if ($is_logged_in->isLoggedIn()) {
-    // We logged in.. yay!
     $page = new \Template(config: $config);
     $page->setTemplate("admin/index.tpl.php");
     $page->set(name: "site_version", value: SENTIMENTAL_VERSION);
     $page->set(name: "username", value: $is_logged_in->getLoggedInUsername());
-    $page->echoToScreen();
+    $inner = $page->grabTheGoods();
+
+    $layout = new \Template(config: $config);
+    $layout->setTemplate("layout/admin_base.tpl.php");
+    $layout->set("page_title", "Workers");
+    $layout->set("page_content", $inner);
+    $layout->echoToScreen();
     exit;
 } else {
     header(header: "Location: /login/");
