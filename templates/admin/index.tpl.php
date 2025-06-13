b25e1f6f5ea4d4fd4d00f069e7feb5d2732c3303
Rob Nugen <rob@robnugen.com>
2025-06-13 11:43:15 +0900

    Show what migrations haven't been applied

--- templates/admin/index.tpl.php
+++ templates/admin/index.tpl.php
@@ -3,6 +3,17 @@
 </div>
 <h1>Welcome to the MarbleTrack3 Admin Dashboard</h1>
 <p>This page can show numbers of workers, parts, snippets, etc</p>
+
+<?php
+if ($has_pending_migrations) {
+        echo "<h3>Pending DB Migrations</h3><ul>";
+        foreach ($pending_migrations as $migration) {
+            echo "<li>$migration <button onclick=\"applyMigration('$migration')\">Apply</button></li>";
+        }
+        echo "</ul>";
+    }
+?>
+
 <div class="PagePanel">
     <a href="/logout/">Logout</a> <br />
 </div>
